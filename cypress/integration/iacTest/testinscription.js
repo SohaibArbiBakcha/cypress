4340ac376ac9947d7066fc6cff444d26461b19a5
SohaibArbiBakcha <bakchaloni@gmail.com>
2021-09-23 17:47:44 +0100

    add more thing to cypress

--- cypress/integration/iacTest/testinscription.js
+++ cypress/integration/iacTest/testinscription.js
@@ -2,8 +2,23 @@ describe("render inscription page", () => {
   //   cy.viewport(1600, 800);
   it("it render correctly", () => {
     cy.visit("/prospect");
-    cy.get('a[href="#/prospect"]').click();
-    cy.get(":nth-child(1) > .column-name").click();
+    cy.get('a[href="#/prospect"]').should("exist");
+
+    cy.get("#status").click();
+    cy.get('[aria-label="Clear value"]').click();
+    cy.scrollTo("bottom");
+    cy.get("#status").click();
+    cy.get('[data-value="treated"]').click();
+    cy.scrollTo("top");
+    cy.get("#status").click();
+    cy.get('[data-value="pending"]').click();
+    cy.get(".jss78 > .MuiButtonBase-root > .MuiButton-label").click();
+
+    cy.get('[data-key="comment"]').click();
+    cy.wait(2000);
+    cy.get("#comment").type("TEST");
+
+    // cy.get(".Mui-selected").click();
     // cy.get('a[href="#/student"]').click();
     // cy.get('a[href="#/classes"]').click();
     // cy.get('a[href="#/courses"]').click();
